0520c56763dc098cb796ae5147e882324c183ad5
damiao tenorio <damiaotenorio@gmail.com>
2021-01-13 19:04:53 -0300

    aulas iniciais

---
+++ udemypy/Aula1/aula1.py
@@ -0,0 +1,10 @@
+print("linha 1")
+print("linha 2")
+ #falando sobre a linha
+ #outra linha
+print("linha 3")
+print("linha 4")  #isso é um comentario
+"""
+print("linha 5")
+print("linha 6")
+"""
---
+++ udemypy/aula2/aula2.py
@@ -0,0 +1,11 @@
+
+#print(123456)
+#print('damiao', 'tenorio')
+
+print('damiao', 'tenorio', sep='-', end=' ')
+print('João', 'e', 'Maria', sep='-')  #end nsere um argumento ao final, no caso deste codigo tira a quebra de linha
+
+print('058', '207', '417', sep='.', end='-')
+print('73')
+
+
