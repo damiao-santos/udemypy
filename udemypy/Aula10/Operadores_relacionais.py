2d250b8f881bf22269872f4cce038dd908fc2261
damiao-santos <59036577+damiao-santos@users.noreply.github.com>
2021-01-18 17:31:21 -0300

    finalizando aula 10

--- udemypy/Aula10/Operadores_relacionais.py
+++ udemypy/Aula10/Operadores_relacionais.py
@@ -11,7 +11,45 @@ Operadores Relacionais
 Eles são feitos para realizar comparações ou relações entre dados, sempre que forem executados eles retornam um
 valor booleano ( true ou false)
 
-parei em 9:04
+caso queiramos fazer uma checagem se uma idade é maior que 18 para pegar emprestimo
+
+ex: name = input('Qual o seu nome: ')
+idade = int(input('Qual a sua idade: '))
+idade_limite = 18
+
+if idade >= idade_limite:
+    print(f'{name} pode pegar emprestimo')
+else:
+    print(f'{name} não pode emprestimo')
+
+
+e se quisermos limitar entre 20 e 30 anos?
+
+name = input('Qual o seu nome: ')
+idade = int(input('Qual a sua idade: '))
+
+#limite para emprestimo
+idade_menor = 20 #muito jovem
+idade_maior = 30 #muito velho
+
+if idade >= idade_menor and idade <= idade_maior:
+    print(f'{name} pode pegar emprestimo')
+else:
+    print(f'{name} não pode emprestimo')
+
 '''
 
-print(2 == 2)
+name = input('Qual o seu nome: ')
+idade = int(input('Qual a sua idade: '))
+
+#limite para emprestimo
+idade_menor = 20 #muito jovem
+idade_maior = 30 #muito velho
+
+if idade >= idade_menor and idade <= idade_maior:
+    print(f'{name} pode pegar emprestimo')
+else:
+    print(f'{name} não pode emprestimo')
+
+
+
